402c85519a83bd10f8285224f071a5decfed19e7
unknown <soccer_Matt_4@yahoo.com>
2019-05-31 10:57:06 -0500

    Created the project repository and created the project file with initial class.

---
+++ SuperheroProject/Models/Superhero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroProject.Models
+{
+    public class Superhero
+    {
+        public int Id { get; set; }
+        public string superheroName { get; set; }
+        public string alterEgoFirstName { get; set; }
+        public string alterEgoLastName { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchphrase { get; set; }
+    }
+}
